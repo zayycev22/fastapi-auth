6049c3880ebba352c00704c4680303228f1486c7
zayycev22 <zayycev22@gmail.com>
2024-03-01 20:39:04 +0300

    refactoring

--- fastapi_auth/sqlalchemy_models/__init__.py
+++
@@ -1,4 +0,0 @@
-from fastapi_auth.sqlalchemy_models.repositories import TokenRepository, UserRepository
-from fastapi_auth.sqlalchemy_models.models import EmailUser, BaseUser, User, Token, ExModel
-
-__all__ = ['TokenRepository', 'UserRepository', 'EmailUser', 'BaseUser', 'User', 'Token', 'ExModel']
--- fastapi_auth/tortoise_models/__init__.py
+++
@@ -1,4 +0,0 @@
-from fastapi_auth.tortoise_models.models import Token, User, EmailUser, BaseUser, ExModel
-from fastapi_auth.tortoise_models.repositories import UserRepository, TokenRepository
-
-__all__ = ['TokenRepository', 'UserRepository', 'EmailUser', 'BaseUser', 'User', 'Token', 'ExModel']
