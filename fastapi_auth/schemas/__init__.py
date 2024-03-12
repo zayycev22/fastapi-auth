5eeb36da07ef722a91108a65eb0d6ebb51c84353
zayycev22 <zayycev22@gmail.com>
2024-03-12 03:35:21 +0300

    version 0.1.2 added schema_detail_type

--- fastapi_auth/schemas/__init__.py
+++ fastapi_auth/schemas/__init__.py
@@ -1,3 +1,3 @@
-from fastapi_auth.schemas.default_schemas import DefaultSchema, AbstractDefaultSchema
+from fastapi_auth.schemas.default_schemas import DefaultSchema, AbstractDefaultSchema, schema_detail_type
 
-__all__ = ['DefaultSchema', "AbstractDefaultSchema"]
+__all__ = ['DefaultSchema', "AbstractDefaultSchema", "schema_detail_type"]
